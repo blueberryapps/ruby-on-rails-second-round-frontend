873b03cd7d39f850ba2c8916e7f9df48154d88fb
Jakub Kottnauer <jk@jakubkottnauer.com>
2016-05-24 15:17:09 +0200

    Disable CORS in fetch

--- app/utils/request.js
+++ app/utils/request.js
@@ -9,7 +9,10 @@ import 'whatwg-fetch';
  * @return {object}           An object containing either "data" or "err"
  */
 export default function request(url, options) {
-  return fetch(url, options)
+  const opt = options || {
+    mode: 'no-cors',
+  };
+  return fetch(url, opt)
     .then(checkStatus)
     .then(parseJSON)
     .then((data) => ({ data }));
